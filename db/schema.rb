6dfc747273d6ffd54b87bdefbfa81743eead5689
Ha Phuong <haphuong191113@gmail.com>
2025-07-25 15:10:09 +0700

    Chapter 9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_22_101951) do
+ActiveRecord::Schema[7.0].define(version: 2025_07_25_003730) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_22_101951) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.date "birthday"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
