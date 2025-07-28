b2af553f52c4f1083d59e98756551a69a171b893
Ha Phuong <haphuong191113@gmail.com>
2025-07-28 22:05:59 +0700

    Chapter 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,13 @@
 class SessionsController < ApplicationController
+  before_action :check_login, only: %i(create)
+  before_action :check_activation, only: %i(create)
+
   # GET /login
   def new; end
 
   # POST /login
   def create
-    user = User.find_by email: params.dig(:session, :email)&.downcase
-    if user&.authenticate params.dig(:session, :password)
-      handle_successful_login user
-    else
-      flash.now[:danger] = t(".invalid_email_or_password")
-      render :new, status: :unprocessable_entity
-    end
+    handle_successful_login @user
   end
 
   # DELETE /logout
@@ -18,18 +15,33 @@ class SessionsController < ApplicationController
     log_out
     redirect_to root_path, status: :see_other
   end
-end
 
-private
+  private
 
-def handle_successful_login user
-  forwarding_url = session[:forwarding_url]
-  reset_session
-  if params.dig(:session, :remember_me) == Settings.remember_me_status
-    remember(user)
-  else
-    forget(user)
+  def handle_successful_login user
+    forwarding_url = session[:forwarding_url]
+    reset_session
+    if params.dig(:session, :remember_me) == Settings.remember_me_status
+      remember(user)
+    else
+      forget(user)
+    end
+    log_in user
+    redirect_to forwarding_url || user, status: :see_other
   end
-  log_in user
-  redirect_to forwarding_url || user
+
+  def check_login
+    @user = User.find_by email: params.dig(:session, :email)&.downcase
+    return if @user&.authenticate params.dig(:session, :password)
+
+    flash.now[:danger] = t(".invalid_email_or_password")
+    render :new, status: :unprocessable_entity
+  end
+
+  def check_activation
+    return if @user.activated?
+
+    flash[:warning] = t(".not_activated")
+    redirect_to root_path, status: :see_other
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
     resources :users, only: %i(show edit update index destroy)
+    resources :account_activations, only: :edit
   end
 end
